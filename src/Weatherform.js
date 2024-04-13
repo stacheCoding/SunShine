e7748e242ca26fe1ba9c9c8913d77cfb0dc0d772
Stache Coding <danieldelaloza@gmail.com>
2024-04-13 11:01:46 -0700

    Added tailwind styles to weatherform

--- src/Weatherform.js
+++ src/Weatherform.js
@@ -24,29 +24,53 @@ function WeatherForm({
   };
 
   return (
-    <form>
+    <form className="flex flex-col items-center">
+      <h1 className="borel-regular text-center text-5xl md:text-6xl lg:text-7xl mt-14">
+        SunShine
+      </h1>
       <input
         type="text"
         value={location}
+        className="input-black py-2 px-3 mt-2 mb-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
         onChange={(e) => onLocationChange(e.target.value)}
         placeholder="Enter location"
       />
-      <button type="button" onClick={handleGetWeather}>
+      <button
+        type="button"
+        className="relative my-2 px-6 py-2 text-base font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purplse-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500 md:w-auto"
+        onClick={handleGetWeather}
+      >
         Get Weather
       </button>
       <div>
-        <button type="button" onClick={() => handleUnitsChange("metric")}>
+        <button
+          type="button"
+          className="relative my-4 mr-2 relative px-6 py-2 text-base font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500 md:w-auto"
+          onClick={() => handleUnitsChange("metric")}
+        >
           Metric
         </button>
-        <button type="button" onClick={() => handleUnitsChange("imperial")}>
+        <button
+          type="button"
+          className="relative px-6 py-2 text-base font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500 md:w-auto"
+          onClick={() => handleUnitsChange("imperial")}
+        >
           Imperial
         </button>
       </div>
       <div>
-        <button type="button" onClick={() => handlePeriodChange("1d")}>
+        <button
+          type="button"
+          className="relative my-4 mr-2 relative px-6 py-2 text-base font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500 md:w-auto"
+          onClick={() => handlePeriodChange("1d")}
+        >
           Daily
         </button>
-        <button type="button" onClick={() => handlePeriodChange("1h")}>
+        <button
+          type="button"
+          className="relative px-6 py-2 text-base font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500 md:w-auto"
+          onClick={() => handlePeriodChange("1h")}
+        >
           Hourly
         </button>
       </div>
